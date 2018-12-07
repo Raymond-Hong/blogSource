9fc00839adcf4d6af694b0003281a15889e64afb
raymond <honxidan@163.com>
2018-12-07 23:29:22 +0800

    update

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,30 +1,22 @@
 import touch from './public/js/vue-touch'
+import dir from './public/js/directive'
+import {zh_tran} from './public/js/zh'
 export default ({
     Vue, // VuePress 正在使用的 Vue 构造函数
     options, // 附加到根实例的一些选项
     router, // 当前应用的路由实例
     siteData // 站点元数据
   }) => {
-    Vue.directive("swipeleft",{
-      bind:function(el,binding){
-        new touch.swipe(el,binding,"swipeleft");
+    dir.derection.forEach((el)=>{
+      if(dir[el]){
+        Vue.directive(el,dir[el]());
+      }else{
+        Vue.directive(el,dir.touchFn(touch.swipe,el));
       }
-    });
-    Vue.directive("swiperight",{
-      bind:function(el,binding){
-        new touch.swipe(el,binding,"swiperight");
-      }
-    });
-    Vue.directive("swipedown",{
-      bind:function(el,binding){
-        new touch.swipe(el,binding,"swipedown");
-      }
-    });
-    Vue.directive("swipeup",{
-      bind:function(el,binding){
-        new touch.swipe(el,binding,"swipeup");
-      }
-    });
+    })
+    Vue.filter('trans',value=>{
+      return zh_tran(value, localStorage.getItem('lang'));
+    })
     // let hello = {
     //   path: '/hello',
     //   name: 'HelloWorld',
